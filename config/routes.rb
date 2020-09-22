3f96c45c4fd13412e5c146eb356e6fd37b22ccd1
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2020-09-22 16:55:46 +0900

    modified

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root "home#index"
+  devise_for :users
+  resources :photos do
+    resource :like
+    resources :comments
+  end
+  resources :users do
+    resource :follow
+    resources :followings
+    resources :followers
+    resources :likes
+  end
+end
